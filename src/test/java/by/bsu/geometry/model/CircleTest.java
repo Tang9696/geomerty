8dd820aa24a3b64b41b5362b85253c31992fd4af
Tang9696 <tangchao9696@gmail.com>
2021-12-09 16:27:57 +0300

    Initial commit

--- src/test/java/by/bsu/geometry/model/CircleTest.java
+++ src/test/java/by/bsu/geometry/model/CircleTest.java
@@ -8,35 +8,7 @@ import org.junit.Test;
 
 public class CircleTest {
 
-    @Test
-    public void testArea(){
-        Circle circle = new Circle(1,1,1);
-        double area = circle.calculateArea();
-        Assert.assertEquals(3.14,area,0.01);
 
-        Rectangle rectangle = new Rectangle(3,9);
-        double area1 = rectangle.calculateArea();
-        Assert.assertEquals(27,area1,0);
-
-        Triangle triangle = new Triangle(3,4,5);
-        double area2 = triangle.calculateArea();
-        Assert.assertEquals(6,area2,0);
-
-    }
-    @Test
-    public void testPerimeter() {
-        Circle circle = new Circle(1, 1, 1);
-        double perimeter = circle.calculatePerimeter();
-        Assert.assertEquals(6.28, perimeter, 0.03);
-
-        Rectangle rectangle = new Rectangle(3, 9);
-        double perimeter1 = rectangle.calculatePerimeter();
-        Assert.assertEquals(24, perimeter1, 0);
-
-        Triangle triangle = new Triangle(3, 4, 5);
-        double perimeter2 = triangle.calculatePerimeter();
-        Assert.assertEquals(12, perimeter2, 0);
-    }
     @Test
     public void testCircleArea(){
         Circle circle = new Circle(1,1,1);
